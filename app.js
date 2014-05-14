c50492d562a6d06477f39dff5f9ab9fcb7393b58
Michael Prouty <michael.prouty@gmail.com>
2014-05-14 17:05:24 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+(function(){
+  var app = angular.module("pokedex", [ ]);
+
+  app.controller("PokedexController", function(){
+    this.pokemons = pokemons;
+  });
+
+  var pokemons = [{
+    name: 'Bulbasaur',
+    type: 'Grass',
+    weight: 15.2,
+    description: "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.",
+  },
+  {
+    name: 'Jigglypuff',
+    type: 'Normal',
+    weight: 12.2,
+    description: "It captivates foes with its huge, round eyes, then lulls them to sleep by singing a soothing melody.",
+  }];
+})();
