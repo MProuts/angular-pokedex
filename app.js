898ca3100f36de73331fc190046f5bc7ba1d27d0
Michael Prouty <michael.prouty@gmail.com>
2014-05-26 17:47:39 -0400

    post working, DOM updating

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 (function(){
   var app = angular.module("pokedex", [ ]);
 
-  app.controller("PokemonController", [ '$http', '$log', function($http, $log){
+  app.controller("PokemonController", [ '$scope', '$http', '$log', function($scope, $http, $log){
     var pokedex = this;
     pokedex.pokemons = [];
+    loadData();
 
-    $http.get('http://localhost:3000/pokemon.json').success(function(data){
-      $log.log(data);
-      pokedex.pokemons = data;
-    });
+    $scope.refresh = function() {
+      loadData();
+      alert("refreshed!");
+    };
 
+    function loadData() {
+      $http.get('http://localhost:3000/pokemon.json').success(function(data){
+        pokedex.pokemons = data;
+      });
+    };
   } ]);
 
   app.controller("TabsController", function(){
@@ -24,35 +30,45 @@
     };
   });
 
-  app.controller("CommentController", [ '$http', '$log', function($http, $log){
+  app.controller("CommentController", [ '$scope', '$http', '$log', function($scope, $http, $log){
     this.comment = {};
+    var that = this;
+
     this.addComment = function(pokemon){
-      pokemon.comments.push(this.comment);
       var mydata = { comment: this.comment };
       var myurl = 'http://localhost:3000/pokemon/' + pokemon.id + '/comments';
       $log.log(mydata);
       $log.log(myurl);
-      $.post(myurl, mydata, 'jsonp')
+      $.ajax({
+        type: "post",
+        url: myurl,
+        data: mydata,
+        dataType: "json",
+        success: function(data){
+          //assign primary key
+          mydata.comment.id = data.id;
+
+          pokemon.comments.push(mydata.comment);
+
+          //refresh the DOM
+          $scope.$apply();
+        }
+      });
       this.comment = {};
     }
 
     this.deleteComment = function(pokemon, comment){
       var myurl = 'http://localhost:3000/pokemon/' + pokemon.id + '/comments/' + comment.id
       $log.log(myurl);
-      //$.ajax({
-      //  url: myurl,
-      //  type: 'post',
-      //  dataType: 'jsonp',
-      //  data: {"_method":"delete"}
-      //});
-     $.ajax({
-       url: myurl,
-       dataType: "json",
-       type: "DELETE",
-       contentType: "application/json",
-     });
+      $.ajax({
+        url: myurl,
+        dataType: "json",
+        type: "DELETE",
+        contentType: "application/json",
+      });
+      comment.deleted = true;
     }
-  } ]);
+  }]);
 
   app.directive("nameAndImage", function(){
     return {
